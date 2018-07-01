770364a6e25a9da5e9a390e8ccb7e91fda93a7ba
MCL <1204172445@qq.com>
2018-07-01 20:23:02 +0800

    -- 测试自动转化开始路径

--- editConf.py
+++ editConf.py
@@ -53,30 +53,70 @@ def _init_conf(cf):
 def _init_qss(cf):
     value = """
 QPushButton{
-        border-style: outset;    
-        background: qradialgradient(spread:reflect, cx:0.5, cy:0.5, radius:1, fx:0.5, fy:0.5, 
-        stop:0.45 rgb(0, 170, 200, 50), 
-        stop:0.49 rgb(100, 100, 100)
-        stop:0.5 rgba(255, 255, 255, 0));
-        background: transparent;
-    }
-    
-    QPushButton:hover{
+    border-style: outset;    
+    background: qradialgradient(spread:reflect, cx:0.5, cy:0.5, radius:1, fx:0.5, fy:0.5, 
+    stop:0.45 rgb(0, 170, 200, 50), 
+    stop:0.49 rgb(100, 100, 100)
+    stop:0.5 rgba(255, 255, 255, 0));
+    background: transparent;
+}
+
+QPushButton:hover{
     background-color :qradialgradient(spread:reflect, cx:0.5, cy:0.5, radius:1, fx:0.5, fy:0.5, 
-        stop:0.45 #09bb07, 
-        stop:0.49 rgb(100, 100, 100)
-        stop:0.5 rgba(255, 255, 255, 0));
-    }
-    
-    QPushButton:checked{
-        background-color:qradialgradient(spread:reflect, cx:0.5, cy:0.5, radius:1, fx:0.5, fy:0.5, 
-        stop:0.45  #09cc07,
-        stop:0.49 rgb(100, 100, 100)
-        stop:0.5 rgba(255, 255, 255, 0));
-    }
-    
+    stop:0.45 #09bb07, 
+    stop:0.49 rgb(100, 100, 100)
+    stop:0.5 rgba(255, 255, 255, 0));
+}
+
+QPushButton:checked{
+    background-color:qradialgradient(spread:reflect, cx:0.5, cy:0.5, radius:1, fx:0.5, fy:0.5, 
+    stop:0.45  #09cc07,
+    stop:0.49 rgb(100, 100, 100)
+    stop:0.5 rgba(255, 255, 255, 0));
+}
+    """
+    menuVal = """
+QMenu{background:purple;
+    border:1px solid lightgray;
+    border-color:green;
+}
+QMenu::item{padding:0px 40px 0px 20px;
+    height:30px;
+    color:blue;
+    background:white;
+    margin:1px 0px 0px 0px;
+}
+
+QMenu::item:selected:enabled{background:lightgray;
+    color:white;
+}
+QMenu::item:selected:!enabled{background:transparent;}
+
+QMenu::separator{height:50px;
+    width:1px;
+    background:white;
+    margin:1px 1px 1px 1px;
+}
+
+QMenu#menu{background:white;
+    border:1px solid lightgray;
+}
+QMenu#menu::item{padding:0px 40px 0px 30px;
+    height:25px;
+}
+QMenu#menu::item:selected:enabled{background:lightgray;
+    color:white;
+}
+QMenu#menu::item:selected:!enabled{background:transparent;}
+
+QMenu#menu::separator{height:1px;
+    background:lightgray;
+    margin:2px 0px 2px 0px;
+}
+QMenu#menu::indicator {padding:10px;}
     """
     cf.set('qss', 'pushbutton', value)
+    cf.set('qss', 'menu', menuVal)
 
 
 def _main():
